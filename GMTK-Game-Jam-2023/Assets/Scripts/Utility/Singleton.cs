7e49ae0bb8a5558feb78942ba1f636371d8e9415
Will Ozeas <willatme@me.com>
2023-07-07 22:52:37 -0400

    Hopefully fixed utility folder

---
+++ GMTK-Game-Jam-2023/Assets/Scripts/Utility/Singleton.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    
+    private static T _instance;
+
+    public bool DontDestroy = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>(); //check for other instances.
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject { name = typeof(T).Name };
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if(DontDestroy) {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if(_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
